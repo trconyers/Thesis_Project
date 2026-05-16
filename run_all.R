4c0d5ecc7cd2a786777b28058f4ec2b4482e2283
trconyers <154757585+trconyers@users.noreply.github.com>
2026-05-16 14:48:07 -0700

    Update run_all.R

--- run_all.R
+++ run_all.R
@@ -23,4 +23,4 @@ source("Scripts/04b_go_enrichment_GSEA.R")
 # 5. Theoretical Synthesis: Resistance vs. Tolerance
 source("Scripts/05_Tolerance_Resistance.R")
 
-print("Pipeline execution complete. Plots saved to /results/Graphs and tables saved to /results/Tables.")
+print("Pipeline execution complete. Plots saved to /Results/Graphs and tables saved to /Results/Tables.")
