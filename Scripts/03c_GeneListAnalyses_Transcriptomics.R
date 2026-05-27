80a614f036b5e8d14e1b276fd17f8db579d8367c
trconyers <tconyers@csu.fullerton.edu>
2026-05-26 20:56:37 -0700

    Update 03c_GeneListAnalyses_Transcriptomics.R

--- Scripts/03c_GeneListAnalyses_Transcriptomics.R
+++ Scripts/03c_GeneListAnalyses_Transcriptomics.R
@@ -5,7 +5,7 @@ library(writexl)
 
 Gene_Table <- as.data.frame(read_excel("Data/Gene_Table.xlsx"))
 Gene_Table.T <- Gene_Table[Gene_Table$Sequencing=="Transcriptomic",]
-FB_invs <- as.data.frame(read_tsv("Scripts/misc/withdrawns.txt"))
+FB_invs <- as.data.frame(read_tsv("misc/withdrawns.txt", skip = 10))
 Gene_Table.T <-Gene_Table.T[!Gene_Table.T$`Candidate FB IDs` %fin% FB_invs$FBIDs,]
 rm(FB_invs)
 
